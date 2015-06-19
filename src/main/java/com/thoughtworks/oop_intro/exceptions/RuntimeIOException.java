4be966f3485e0a16c3cd092efc8023ed4bcb2228
Bill Schofield <bill@billschofield.net>
2015-06-19 14:15:56 -0500

    Removed unneeded project version and import

--- src/main/java/com/thoughtworks/oop_intro/exceptions/RuntimeIOException.java
+++ src/main/java/com/thoughtworks/oop_intro/exceptions/RuntimeIOException.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.oop_intro.exceptions;
 
-import java.io.IOException;
 
 public class RuntimeIOException extends RuntimeException {
     public RuntimeIOException(Exception e) {
